f6e85763d17c3ef8f3f9caefcee299005863194e
enzo <enzoatm5@hotmail.com>
2024-11-19 17:51:56 -0300

    atualizacao 19-11-2024

--- config PATH (sem acesso adm).bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=C:\Users\enzo.albuquerque\AppData\Local\Programs\Python\Python312;C:\Users\enzo.albuquerque\AppData\Local\Programs\Python\Python312\Scripts; C:\Users\enzo.albuquerque\Documents\Enzo\programacao\BIMPlan\plan\Scripts;%PATH%
-cmd
